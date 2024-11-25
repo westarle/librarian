122c664f828fac3172ca2d2afbaad0f75b4f017e
Julie Qiu <julieqiu@google.com>
2024-11-25 16:52:20 -0500

    impl(internal/generate): clone repos inside generator
    The generator now clones googleapis and google-cloud-<language>.

--- internal/generate/run.go
+++ internal/generate/run.go
@@ -16,13 +16,37 @@ package generate
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/googleapis/generator/internal/gitrepo"
 )
 
 func Run(ctx context.Context, arg ...string) error {
 	cfg := &config{}
-	_, err := parseFlags(cfg, arg)
+	cfg, err := parseFlags(cfg, arg)
 	if err != nil {
 		return err
 	}
+	if _, err := cloneGoogleapis(ctx); err != nil {
+		return err
+	}
+	if _, err := cloneLanguageRepo(ctx, cfg.language); err != nil {
+		return err
+	}
 	return nil
+}
+
+const googleapisURL = "https://github.com/googleapis/googleapis"
+
+func cloneGoogleapis(ctx context.Context) (*gitrepo.Repo, error) {
+	repoPath := filepath.Join(os.TempDir(), "/generator-googleapis")
+	return gitrepo.CloneOrOpen(ctx, repoPath, googleapisURL)
+}
+
+func cloneLanguageRepo(ctx context.Context, language string) (*gitrepo.Repo, error) {
+	languageRepoURL := fmt.Sprintf("https://github.com/googleapis/google-cloud-%s", language)
+	repoPath := filepath.Join(os.TempDir(), fmt.Sprintf("/google-cloud-%s", language))
+	return gitrepo.CloneOrOpen(ctx, repoPath, languageRepoURL)
 }
