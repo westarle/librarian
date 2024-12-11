e0a63d80c72480d7588d63ef717e20b15b5870ed
Burke Davison <burkedavison@google.com>
2024-12-11 19:17:15 +0000

    chore: use FlagSet.StringVar variant for language flag declaration

--- internal/command/flags.go
+++ internal/command/flags.go
@@ -31,11 +31,11 @@ var (
 )
 
 func addFlagAPIRoot(fs *flag.FlagSet) {
-	fs.StringVar(&flagAPIRoot, "api-root", "", "location of googleapis repository")
+	fs.StringVar(&flagAPIRoot, "api-root", "", "(Required) location of googleapis repository")
 }
 
 func addFlagAPIPath(fs *flag.FlagSet) {
-	fs.StringVar(&flagAPIPath, "api-path", "", "path api-root to the API to be generated (e.g., google/cloud/functions/v2)")
+	fs.StringVar(&flagAPIPath, "api-path", "", "(Required) path api-root to the API to be generated (e.g., google/cloud/functions/v2)")
 }
 
 func addFlagBranch(fs *flag.FlagSet) {
@@ -43,13 +43,7 @@ func addFlagBranch(fs *flag.FlagSet) {
 }
 
 func addFlagLanguage(fs *flag.FlagSet) {
-	fs.Func("language", "language to generate code for", func(language string) error {
-		if !supportedLanguages[language] {
-			return fmt.Errorf("invalid -language flag specified: %q", language)
-		}
-		flagLanguage = language
-		return nil
-	})
+  fs.StringVar(&flagLanguage, "language", "", "(Required) language to generate code for")
 }
 
 var supportedLanguages = map[string]bool{
