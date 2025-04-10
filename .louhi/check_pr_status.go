74f25346f78ada455dc410d8580bfd50bfecf63c
ldetmer <1771267+ldetmer@users.noreply.github.com>
2025-04-10 15:10:20 -0400

    fix: fix logic to handle PR that is already merged (#126)
    * fix: fix logic to handle PR that is already merged
    
    * added more verbose logging for mergable status

--- .louhi/check_pr_status.go
+++ .louhi/check_pr_status.go
@@ -53,11 +53,11 @@ func checkPRStatus(prNumber int, repoOwner string, repoName string, statusCheck 
 				time.Sleep(pollInterval)
 			}
 		} else if statusCheck == "mergeable" {
-			if pr.GetMergeable() {
-				slog.Info("PR is mergable")
+			if pr.GetMergeable() || pr.GetMerged() {
+				slog.Info("PR is mergable or already merged", "mergeable status", pr.GetMergeable(), "merged status", pr.GetMerged())
 				return
 			} else {
-				slog.Info("PR is not mergable, will try again", "mergeable status", pr.GetMerged())
+				slog.Info("PR is not mergable, will try again", "mergeable status", pr.GetMergeable(), "merged status", pr.GetMerged())
 				time.Sleep(pollInterval)
 			}
 		}
