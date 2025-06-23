ac87be6c66fbab50d2b4a7c3fa037f75d6e94d10
Julie Qiu <julieqiu@google.com>
2025-06-23 10:19:26 -0400

    feat(all): check that exported symbols have doc comments (#523)
    Add a test to ensure that exported funcs, types, consts, and vars must
    have comments starting with their name.
    
    This test is skipped until
    https://github.com/googleapis/librarian/issues/521 is addressed.
    
    For https://github.com/googleapis/librarian/issues/522

--- all_test.go
+++ all_test.go
@@ -17,9 +17,13 @@ package main_test
 import (
 	"bufio"
 	"errors"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"io/fs"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"regexp"
 	"slices"
 	"strings"
@@ -114,5 +118,59 @@ func rungo(t *testing.T, args ...string) {
 			t.Fatalf("%v: %v\n%s", cmd, err, ee.Stderr)
 		}
 		t.Fatalf("%v: %v\n%s", cmd, err, output)
+	}
+}
+
+func TestExportedSymbolsHaveDocs(t *testing.T) {
+	// TODO(https://github.com/googleapis/librarian/issues/522): turn on once
+	// all existing symbols have docs.
+	t.Skip()
+
+	err := filepath.WalkDir(".", func(path string, d os.DirEntry, err error) error {
+		if err != nil || d.IsDir() || !strings.HasSuffix(path, ".go") || strings.HasSuffix(path, "_test.go") {
+			return nil
+		}
+
+		fset := token.NewFileSet()
+		node, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
+		if err != nil {
+			t.Errorf("failed to parse file %q: %v", path, err)
+			return nil
+		}
+
+		// Visit every top-level declaration in the file.
+		for _, decl := range node.Decls {
+			gen, ok := decl.(*ast.GenDecl)
+			if ok && (gen.Tok == token.TYPE || gen.Tok == token.VAR) {
+				for _, spec := range gen.Specs {
+					switch s := spec.(type) {
+					case *ast.TypeSpec:
+						checkDoc(t, s.Name, s.Doc, path)
+					case *ast.ValueSpec:
+						for _, name := range s.Names {
+							checkDoc(t, name, gen.Doc, path)
+						}
+					}
+				}
+			}
+			if fn, ok := decl.(*ast.FuncDecl); ok {
+				checkDoc(t, fn.Name, fn.Doc, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func checkDoc(t *testing.T, name *ast.Ident, doc *ast.CommentGroup, path string) {
+	t.Helper()
+	if !name.IsExported() {
+		return
+	}
+	if doc == nil {
+		t.Errorf("%s: %q is missing doc comment",
+			path, name.Name)
 	}
 }
