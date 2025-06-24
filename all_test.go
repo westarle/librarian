f88310179258de5e6b6f33ab8eac0004b41dff59
Jon Skeet <skeet@pobox.com>
2025-06-24 12:41:55 +0100

    fix(all_test.go): make the linter ignore generated protobuf code (#588)
    Fixes #587

--- all_test.go
+++ all_test.go
@@ -127,7 +127,8 @@ func TestExportedSymbolsHaveDocs(t *testing.T) {
 	t.Skip()
 
 	err := filepath.WalkDir(".", func(path string, d os.DirEntry, err error) error {
-		if err != nil || d.IsDir() || !strings.HasSuffix(path, ".go") || strings.HasSuffix(path, "_test.go") {
+		if err != nil || d.IsDir() || !strings.HasSuffix(path, ".go") ||
+			strings.HasSuffix(path, "_test.go") || strings.HasSuffix(path, ".pb.go") {
 			return nil
 		}
 
