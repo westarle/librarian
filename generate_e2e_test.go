7b4437a0f1ab2830587a56a854b3a5edb9490a98
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2025-08-04 17:35:51 +0000

    chore: create end-to-end tests for configure command (#1004)
    Fixes #862

--- generate_e2e_test.go
+++ generate_e2e_test.go
@@ -20,19 +20,22 @@ package librarian
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
-)
 
-const (
-	repo               = "repo"
-	localRepoBackupDir = "testdata/e2e/generate/repo_backup"
-	localAPISource     = "testdata/e2e/generate/api_root"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestRunGenerate(t *testing.T) {
+	const (
+		repo                = "repo"
+		initialRepoStateDir = "testdata/e2e/generate/repo_init"
+		localAPISource      = "testdata/e2e/generate/api_root"
+	)
 	t.Parallel()
 	for _, test := range []struct {
 		name    string
@@ -44,15 +47,15 @@ func TestRunGenerate(t *testing.T) {
 			api:  "google/cloud/pubsub/v1",
 		},
 		{
-			name:    "non existant in api source",
-			api:     "google/invalid/path",
+			name:    "non existent in api source",
+			api:     "google/non-existent/path",
 			wantErr: true,
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
 			workRoot := filepath.Join(t.TempDir())
 			repo := filepath.Join(workRoot, repo)
-			if err := prepareTest(t, repo, workRoot, localRepoBackupDir); err != nil {
+			if err := prepareTest(t, repo, workRoot, initialRepoStateDir); err != nil {
 				t.Fatalf("prepare test error = %v", err)
 			}
 
@@ -96,6 +99,92 @@ func TestRunGenerate(t *testing.T) {
 				if content.ErrorMessage == "" {
 					t.Fatalf("can not find error message in generate response")
 				}
+			}
+		})
+	}
+}
+
+func TestRunConfigure(t *testing.T) {
+	const (
+		localRepoDir        = "testdata/e2e/configure/repo"
+		initialRepoStateDir = "testdata/e2e/configure/repo_init"
+		repo                = "repo"
+	)
+	for _, test := range []struct {
+		name         string
+		api          string
+		library      string
+		apiSource    string
+		updatedState string
+		wantErr      bool
+	}{
+		{
+			name:         "runs successfully",
+			api:          "google/cloud/new-library-path/v2",
+			library:      "new-library",
+			apiSource:    "testdata/e2e/configure/api_root",
+			updatedState: "testdata/e2e/configure/updated-state.yaml",
+		},
+		{
+			name:         "failed due to simulated error in configure command",
+			api:          "google/cloud/another-library/v3",
+			library:      "simulate-configure-error-id",
+			apiSource:    "testdata/e2e/configure/api_root",
+			updatedState: "testdata/e2e/configure/updated-state.yaml",
+			wantErr:      true,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			workRoot := filepath.Join(os.TempDir(), fmt.Sprintf("rand-%d", rand.Intn(1000)))
+			repo := filepath.Join(workRoot, repo)
+			if err := prepareTest(t, repo, workRoot, initialRepoStateDir); err != nil {
+				t.Fatalf("prepare test error = %v", err)
+			}
+
+			cmd := exec.Command(
+				"go",
+				"run",
+				"github.com/googleapis/librarian/cmd/librarian",
+				"generate",
+				fmt.Sprintf("--api=%s", test.api),
+				fmt.Sprintf("--output=%s", workRoot),
+				fmt.Sprintf("--repo=%s", repo),
+				fmt.Sprintf("--api-source=%s", test.apiSource),
+				fmt.Sprintf("--library=%s", test.library),
+			)
+			cmd.Stderr = os.Stderr
+			cmd.Stdout = os.Stdout
+			err := cmd.Run()
+			if test.wantErr {
+				if err == nil {
+					t.Fatal("Configure command should fail")
+				}
+
+				// the exact message is not populated here, but we can check it's
+				// indeed an error returned from docker container.
+				if g, w := err.Error(), "exit status 1"; !strings.Contains(g, w) {
+					t.Errorf("got %q, wanted it to contain %q", g, w)
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("Failed to run configure: %v", err)
+			}
+
+			// Verify the file content
+			gotBytes, err := os.ReadFile(filepath.Join(repo, ".librarian", "state.yaml"))
+			if err != nil {
+				t.Fatalf("Failed to read configure response file: %v", err)
+			}
+
+			wantBytes, readErr := os.ReadFile(test.updatedState)
+			if readErr != nil {
+				t.Fatalf("Failed to read expected state for comparison: %v", readErr)
+			}
+
+			if diff := cmp.Diff(string(wantBytes), string(gotBytes)); diff != "" {
+				t.Errorf("Generated yaml mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
--- testdata/e2e_func.go
+++ testdata/e2e_func.go
@@ -10,13 +10,15 @@ import (
 )
 
 const (
-	inputDir          = "input"
-	librarian         = "librarian"
-	outputDir         = "output"
-	source            = "source"
-	generateRequest   = "generate-request.json"
-	generateResponse  = "generate-response.json"
-	nonExistedLibrary = "non-existed-library"
+	inputDir                 = "input"
+	librarian                = "librarian"
+	outputDir                = "output"
+	source                   = "source"
+	configureRequest         = "configure-request.json"
+	configureResponse        = "configure-response.json"
+	generateRequest          = "generate-request.json"
+	generateResponse         = "generate-response.json"
+	simulateConfigureErrorID = "simulate-configure-error-id"
 )
 
 func main() {
@@ -26,6 +28,10 @@ func main() {
 
 	log.Print("received command: ", os.Args[1:])
 	switch os.Args[1] {
+	case "configure":
+		if err := doConfigure(os.Args[2:]); err != nil {
+			log.Fatal(err)
+		}
 	case "generate":
 		if err := doGenerate(os.Args[2:]); err != nil {
 			log.Fatal(err)
@@ -35,21 +41,56 @@ func main() {
 	}
 }
 
-func doGenerate(args []string) error {
-	request, err := parseRequest(args)
+func doConfigure(args []string) error {
+	request, err := parseConfigureRequest(args)
 	if err != nil {
 		return err
 	}
-	if err := validateLibrarianDir(request.librarianDir); err != nil {
+	if err := validateLibrarianDir(request.librarianDir, configureRequest); err != nil {
 		return err
 	}
-	if err := writeToOutput(request); err != nil {
+
+	library, err := readConfigureRequest(filepath.Join(request.librarianDir, configureRequest))
+	if err != nil {
 		return err
 	}
-	return nil
+
+	return writeConfigureResponse(request, library)
 }
 
-func parseRequest(args []string) (*generateOption, error) {
+func doGenerate(args []string) error {
+	request, err := parseGenerateRequest(args)
+	if err != nil {
+		return err
+	}
+	if err := validateLibrarianDir(request.librarianDir, generateRequest); err != nil {
+		return err
+	}
+
+	return writeToOutput(request)
+}
+
+func parseConfigureRequest(args []string) (*configureOption, error) {
+	configureOption := &configureOption{}
+	for _, arg := range args {
+		option, _ := strings.CutPrefix(arg, "--")
+		strs := strings.Split(option, "=")
+		switch strs[0] {
+		case inputDir:
+			configureOption.intputDir = strs[1]
+		case librarian:
+			configureOption.librarianDir = strs[1]
+		case source:
+			configureOption.sourceDir = strs[1]
+		default:
+			return nil, errors.New("unrecognized option: " + option)
+		}
+	}
+
+	return configureOption, nil
+}
+
+func parseGenerateRequest(args []string) (*generateOption, error) {
 	generateOption := &generateOption{}
 	for _, arg := range args {
 		option, _ := strings.CutPrefix(arg, "--")
@@ -71,24 +112,65 @@ func parseRequest(args []string) (*generateOption, error) {
 	return generateOption, nil
 }
 
-func validateLibrarianDir(dir string) error {
-	if _, err := os.Stat(filepath.Join(dir, generateRequest)); err != nil {
+func validateLibrarianDir(dir, requestFile string) error {
+	if _, err := os.Stat(filepath.Join(dir, requestFile)); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func writeToOutput(option *generateOption) error {
+func readConfigureRequest(path string) (*libraryState, error) {
+	library := &libraryState{}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(data, library); err != nil {
+		return nil, err
+	}
+
+	if library.ID == simulateConfigureErrorID {
+		// Simulate a configure command error
+		return nil, errors.New("simulate configure command error")
+	}
+
+	return library, nil
+}
+
+func writeConfigureResponse(option *configureOption, library *libraryState) error {
+	library = populateAdditionalFields(library)
+	data, err := json.MarshalIndent(library, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	jsonFilePath := filepath.Join(option.librarianDir, configureResponse)
+	jsonFile, err := os.Create(jsonFilePath)
+	if err != nil {
+		return err
+	}
+
+	if _, err := jsonFile.Write(data); err != nil {
+		return err
+	}
+	log.Print("write configure response to " + jsonFilePath)
+
+	return nil
+}
+
+func writeToOutput(option *generateOption) (err error) {
 	jsonFilePath := filepath.Join(option.outputDir, generateResponse)
 	jsonFile, err := os.Create(jsonFilePath)
 	if err != nil {
 		return err
 	}
-	defer jsonFile.Close()
+	defer func() {
+		err = errors.Join(err, jsonFile.Close())
+	}()
 
 	dataMap := map[string]string{}
-	if option.libraryID == nonExistedLibrary {
+	if option.libraryID == simulateConfigureErrorID {
 		dataMap["error"] = "simulated generation error"
 	}
 	data, err := json.MarshalIndent(dataMap, "", "  ")
@@ -98,11 +180,26 @@ func writeToOutput(option *generateOption) error {
 	if _, err := jsonFile.Write(data); err != nil {
 		return err
 	}
-	log.Print("write generate response to " + jsonFilePath)
-	if option.libraryID == nonExistedLibrary {
+	if option.libraryID == simulateConfigureErrorID {
 		return errors.New("generation failed due to invalid library id")
 	}
 	return nil
+}
+
+func populateAdditionalFields(library *libraryState) *libraryState {
+	library.Version = "1.0.0"
+	library.SourceRoots = []string{"example-source-path", "example-source-path-2"}
+	library.PreserveRegex = []string{"example-preserve-regex", "example-preserve-regex-2"}
+	library.RemoveRegex = []string{"example-remove-regex", "example-remove-regex-2"}
+
+	return library
+}
+
+type configureOption struct {
+	intputDir    string
+	librarianDir string
+	libraryID    string
+	sourceDir    string
 }
 
 type generateOption struct {
@@ -111,4 +208,18 @@ type generateOption struct {
 	librarianDir string
 	libraryID    string
 	sourceDir    string
+}
+
+type libraryState struct {
+	ID            string   `json:"id"`
+	Version       string   `json:"version"`
+	APIs          []*api   `json:"apis"`
+	SourceRoots   []string `json:"source_roots"`
+	PreserveRegex []string `json:"preserve_regex"`
+	RemoveRegex   []string `json:"remove_regex"`
+}
+
+type api struct {
+	Path          string `json:"path"`
+	ServiceConfig string `json:"service_config"`
 }
