4b35724fede5d9f202fdd0bb072580198ce38a70
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2025-08-08 20:56:21 +0000

    feat: add a global config (#1663)
    Fixes #1656

---
+++ internal/config/global_config.go
@@ -0,0 +1,51 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"fmt"
+)
+
+// GlobalConfig defines the contract for the config.yaml file.
+type GlobalConfig struct {
+	GlobalFilesAllowlist []*GlobalFile `yaml:"global_files_allowlist"`
+}
+
+// GlobalFile defines the global files in language repositories.
+type GlobalFile struct {
+	Path        string `yaml:"path"`
+	Permissions string `yaml:"permissions"`
+}
+
+var validPermissions = map[string]bool{
+	"read-only":  true,
+	"write-only": true,
+	"read-write": true,
+}
+
+// Validate checks that the GlobalConfig is valid.
+func (g *GlobalConfig) Validate() error {
+	for i, globalFile := range g.GlobalFilesAllowlist {
+		path, permissions := globalFile.Path, globalFile.Permissions
+		if !isValidDirPath(path) {
+			return fmt.Errorf("invalid global file path at index %d: %q", i, path)
+		}
+		if _, ok := validPermissions[permissions]; !ok {
+			return fmt.Errorf("invalid global file permissions at index %d: %q", i, permissions)
+		}
+	}
+
+	return nil
+}
---
+++ internal/config/global_config_test.go
@@ -0,0 +1,102 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGlobalConfig_Validate(t *testing.T) {
+	for _, test := range []struct {
+		name       string
+		config     *GlobalConfig
+		wantErr    bool
+		wantErrMsg string
+	}{
+		{
+			name: "valid config",
+			config: &GlobalConfig{
+				GlobalFilesAllowlist: []*GlobalFile{
+					{
+						Path:        "a/path",
+						Permissions: "read-only",
+					},
+					{
+						Path:        "another/path",
+						Permissions: "write-only",
+					},
+					{
+						Path:        "other/paths",
+						Permissions: "read-write",
+					},
+				},
+			},
+		},
+		{
+			name: "invalid path in config",
+			config: &GlobalConfig{
+				GlobalFilesAllowlist: []*GlobalFile{
+					{
+						Path:        "a/path",
+						Permissions: "read-only",
+					},
+					{
+						Path:        "/another/absolute/path",
+						Permissions: "write-only",
+					},
+				},
+			},
+			wantErr:    true,
+			wantErrMsg: "invalid global file path",
+		},
+		{
+			name: "invalid permission in config",
+			config: &GlobalConfig{
+				GlobalFilesAllowlist: []*GlobalFile{
+					{
+						Path:        "a/path",
+						Permissions: "write-only",
+					},
+					{
+						Path:        "another/path",
+						Permissions: "unknown",
+					},
+				},
+			},
+			wantErr:    true,
+			wantErrMsg: "invalid global file permissions",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			err := test.config.Validate()
+			if test.wantErr {
+				if err == nil {
+					t.Errorf("GlobalConfig.Validate() should return error")
+				}
+
+				if !strings.Contains(err.Error(), test.wantErrMsg) {
+					t.Errorf("GlobalConfig.Validate() err = %v, want error containing %q", err, test.wantErrMsg)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Errorf("GlobalConfig.Validate() error = %v, wantErr %v", err, test.wantErr)
+			}
+		})
+	}
+}
